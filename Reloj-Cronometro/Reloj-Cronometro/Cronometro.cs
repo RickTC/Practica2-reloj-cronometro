953f808088465a21c3d64e462ee4215c90e2dc82
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-17 11:59:37 -0500

    clase cronometro lista.

--- Reloj-Cronometro/Reloj-Cronometro/Cronometro.cs
+++ Reloj-Cronometro/Reloj-Cronometro/Cronometro.cs
@@ -6,6 +6,18 @@ namespace RelojCronometro
 	{
 		public Cronometro ()
 		{
+			int h,m,s;
+			
+				for(h=0; h<100; h++){
+					for(m=0; m<60; m++){
+						for(s=0; s<60; s++){
+							
+							Console.WriteLine(h +":"+ m +":"+ s);
+							System.Threading.Thread.Sleep(1000);
+							Console.Clear();
+					}
+				} 
+			}
 		}
 	}
 }
