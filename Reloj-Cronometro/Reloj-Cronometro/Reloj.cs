1ae7e66c6387f9fc40bdf6b349767abe68652fe6
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-17 11:53:06 -0500

    clase reloj lista.

--- Reloj-Cronometro/Reloj-Cronometro/Reloj.cs
+++ Reloj-Cronometro/Reloj-Cronometro/Reloj.cs
@@ -6,6 +6,22 @@ namespace RelojCronometro
 	{
 		public Reloj ()
 		{
+			
+			int h= DateTime.Now.Hour;
+			int m= DateTime.Now.Minute;
+			int s= DateTime.Now.Second;
+				
+				for(int ho=h; ho<24; ho++){
+					for(int mi=m; mi<60; mi++){
+						for(int se=s; se<60; se++){
+							
+						Console.Clear();
+						Console.WriteLine (ho +":"+mi+":"+ se);
+						System.Threading.Thread.Sleep (1000);
+						
+					}
+				}	
+			 }
 		}
 	}
 }
