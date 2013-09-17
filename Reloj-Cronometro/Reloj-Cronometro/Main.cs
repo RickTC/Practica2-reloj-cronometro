18b16d5fb34d06aabe9b20ff6c6078d9c9014698
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-17 12:00:09 -0500

    clase principal lista.

--- Reloj-Cronometro/Reloj-Cronometro/Main.cs
+++ Reloj-Cronometro/Reloj-Cronometro/Main.cs
@@ -6,7 +6,32 @@ namespace RelojCronometro
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Reloj r;
+			Cronometro c;
+			
+			Console.WriteLine ("Menu");
+			Console.WriteLine("1- Reloj");
+			Console.WriteLine("2- Cronometro");
+			Console.WriteLine("Eliga una opcion");
+			
+				string p = Console.ReadLine();
+					int op = int.Parse(p);
+					
+						switch(op){
+				
+							case 1:
+									r= new Reloj();
+									r.GetHashCode();
+							break;
+							
+							case 2:
+									c = new Cronometro();
+									c.GetHashCode();
+							break;
+							
+							case 3:
+									Console.WriteLine("Opcion NO VALIDA");
+							break;
 		}
 	}
 }
